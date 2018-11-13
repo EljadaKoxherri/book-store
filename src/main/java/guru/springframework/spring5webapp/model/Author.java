ecfa7f71a758c797099621cfb93d207b7071bb8a
EljadaKoxherri <eljadakoxherri@hotmail.com>
2018-11-13 10:34:52 +0100

    Adding Repositories

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -13,7 +14,7 @@ public class Author {
     private String firstName;
     private String lastName;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "authors")
     private Set<Book>books = new HashSet<>();
 
     public Author(){
@@ -52,7 +53,26 @@ public class Author {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
-
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -16,6 +17,8 @@ public class Book {
     private String publisher;
 
     @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")                   )
     private Set<Author> authors = new HashSet<>();
 
     public Book(){
@@ -65,6 +68,27 @@ public class Book {
         this.publisher = publisher;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
+    @Override
+    public String  toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
